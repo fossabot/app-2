3621dd5ba61cca8e937824ef2b2c977ce2c45438
rijkvanzanten <rijkvanzanten@me.com>
2018-05-07 21:03:22 -0400

    Cleanup globals

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,4 @@
 import Vue from "vue";
-import Spinner from "vue-simple-spinner";
 
 import HeaderBar from "./components/header-bar/header-bar.vue";
 import NavSidebar from "./components/nav-sidebar/nav-sidebar.vue";
@@ -9,26 +8,6 @@ import VListing from "./components/extensions/v-listing/v-listing.vue";
 import VListingOptions from "./components/extensions/v-listing-options/v-listing-options.vue";
 import VReadonly from "./components/extensions/v-readonly/v-readonly.vue";
 
-// import InvisibleLabel from "./components-old/globals/InvisibleLabel.vue";
-// import VModal from "./components-old/globals/VModal.vue";
-// import VAvatar from "./components-old/globals/VAvatar.vue";
-// import VAlert from "./components-old/globals/VAlert.vue";
-// import VConfirm from "./components-old/globals/VConfirm.vue";
-// import VPrompt from "./components-old/globals/VPrompt.vue";
-// import VButton from "./components-old/globals/VButton.vue";
-// import VCard from "./components-old/globals/VCard.vue";
-// import VCheckbox from "./components-old/globals/VCheckbox.vue";
-// import VDetails from "./components-old/globals/VDetails.vue";
-// import VInput from "./components-old/globals/VInput.vue";
-// import VRadio from "./components-old/globals/VRadio.vue";
-// import VSelect from "./components-old/globals/VSelect.vue";
-// import VSlider from "./components-old/globals/VSlider.vue";
-// import VTable from "./components-old/globals/VTable.vue";
-// import VTextarea from "./components-old/globals/VTextarea.vue";
-
-// import ReadonlyExtension from "./components-old/extensions/ReadonlyExtension.vue";
-// import ListingOptionsExtension from "./components-old/extensions/ListingOptionsExtension.vue";
-
 Vue.component("header-bar", HeaderBar);
 Vue.component("nav-sidebar", NavSidebar);
 Vue.component("edit-form", EditForm);
@@ -36,22 +15,3 @@ Vue.component("v-interface", VInterface);
 Vue.component("v-listing", VListing);
 Vue.component("v-listing-options", VListingOptions);
 Vue.component("v-readonly", VReadonly);
-
-// Vue.component("invisible-label", InvisibleLabel);
-// Vue.component("v-readonly", ReadonlyExtension);
-// Vue.component("spinner", Spinner);
-// Vue.component("v-avatar", VAvatar);
-// Vue.component("v-alert", VAlert);
-// Vue.component("v-confirm", VConfirm);
-// Vue.component("v-prompt", VPrompt);
-// Vue.component("v-button", VButton);
-// Vue.component("v-card", VCard);
-// Vue.component("v-checkbox", VCheckbox);
-// Vue.component("v-details", VDetails);
-// Vue.component("v-input", VInput);
-// Vue.component("v-modal", VModal);
-// Vue.component("v-radio", VRadio);
-// Vue.component("v-select", VSelect);
-// Vue.component("v-slider", VSlider);
-// Vue.component("v-table", VTable);
-// Vue.component("v-textarea", VTextarea);
